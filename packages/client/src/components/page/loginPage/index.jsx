6f25e1faebae204cb751583fff99264c1bb4dc6e
Dương Trung Quốc <146050305+TrungQuoc109@users.noreply.github.com>
2023-12-11 10:39:33 +0700

    call api login/register

--- packages/client/src/components/page/loginPage/index.jsx
+++ packages/client/src/components/page/loginPage/index.jsx
@@ -1,85 +1,123 @@
 import React, { useState } from "react";
 import {
-  Container,
-  Grid,
-  TextField,
-  Button,
-  Typography,
-  Card,
-  CardContent,
+    Container,
+    Grid,
+    TextField,
+    Button,
+    Typography,
+    Card,
+    CardContent,
 } from "@mui/material";
 import ResponsiveAppBar from "../../Nav-bar";
 import Footer from "../../footer";
 
 const LoginPage = () => {
-  const [formData, setFormData] = useState({
-    username: "",
-    password: "",
-  });
-
-  const handleInputChange = (e) => {
-    const { name, value } = e.target;
-    setFormData({
-      ...formData,
-      [name]: value,
+    const [formData, setFormData] = useState({
+        username: "",
+        password: "",
     });
-  };
+    const [token, setToken] = useState(null);
+    const [errorMessage, setErrorMessage] = useState(null);
+    const handleInputChange = (e) => {
+        const { name, value } = e.target;
+        setFormData({
+            ...formData,
+            [name]: value,
+        });
+    };
+    const saveTokenToLocalStorage = (token) => {
+        localStorage.setItem("jwtToken", token);
+    };
+    const handleSubmit = async (e) => {
+        e.preventDefault();
+        try {
+            const response = await fetch(
+                "http://localhost:8080/account/login",
+                {
+                    method: "POST",
+                    headers: {
+                        "Content-Type": "application/json",
+                    },
+                    body: JSON.stringify(formData),
+                }
+            );
 
-  const handleSubmit = (e) => {
-    e.preventDefault();
-    console.log("Form submitted with:", formData);
-  };
+            if (response.ok) {
+                const data = await response.json();
 
-  return (
-    <Grid>
-      <ResponsiveAppBar />
-      <Container maxWidth="xs">
-        <Card style={{ marginTop: "5rem", marginBottom: "7rem" }}>
-          <CardContent>
-            <Typography variant="h5" align="center" gutterBottom>
-              Login
-            </Typography>
-            <form onSubmit={handleSubmit}>
-              <Grid container spacing={2}>
-                <Grid item xs={12}>
-                  <TextField
-                    fullWidth
-                    label="Username"
-                    name="username"
-                    variant="outlined"
-                    value={formData.username}
-                    onChange={handleInputChange}
-                  />
-                </Grid>
-                <Grid item xs={12}>
-                  <TextField
-                    fullWidth
-                    label="Password"
-                    name="password"
-                    type="password"
-                    variant="outlined"
-                    value={formData.password}
-                    onChange={handleInputChange}
-                  />
-                </Grid>
-                <Grid item xs={12}>
-                  <Button
-                    type="submit"
-                    fullWidth
-                    variant="contained"
-                    color="primary"
-                  >
-                    Login
-                  </Button>
-                </Grid>
-              </Grid>
-            </form>
-          </CardContent>
-        </Card>
-      </Container>
-      <Footer />
-    </Grid>
-  );
+                const mockToken = data.token;
+                setToken(mockToken);
+                saveTokenToLocalStorage(mockToken);
+                //chuyển trang
+            } else {
+                const errorData = await response.json();
+                setErrorMessage(errorData.message);
+            }
+        } catch (error) {
+            console.error("Error during login:", error);
+            setErrorMessage("Internal Server Error");
+        }
+    };
+
+    return (
+        <Grid>
+            <ResponsiveAppBar />
+            <Container maxWidth="xs">
+                <Card style={{ marginTop: "5rem", marginBottom: "7rem" }}>
+                    <CardContent>
+                        <Typography variant="h5" align="center" gutterBottom>
+                            Login
+                        </Typography>
+                        <form onSubmit={handleSubmit}>
+                            <Grid container spacing={2}>
+                                <Grid item xs={12}>
+                                    <TextField
+                                        fullWidth
+                                        label="Username"
+                                        name="username"
+                                        variant="outlined"
+                                        value={formData.username}
+                                        onChange={handleInputChange}
+                                    />
+                                </Grid>
+                                <Grid item xs={12}>
+                                    <TextField
+                                        fullWidth
+                                        label="Password"
+                                        name="password"
+                                        type="password"
+                                        variant="outlined"
+                                        value={formData.password}
+                                        onChange={handleInputChange}
+                                    />
+                                </Grid>
+                                <Grid item xs={12}>
+                                    <Button
+                                        type="submit"
+                                        fullWidth
+                                        variant="contained"
+                                        color="primary"
+                                    >
+                                        Login
+                                    </Button>
+                                </Grid>
+                            </Grid>
+                        </form>
+                        {errorMessage && (
+                            <Typography
+                                variant="body2"
+                                color="error"
+                                align="center"
+                            >
+                                {errorMessage}
+                            </Typography>
+                        )}
+                    </CardContent>
+                </Card>
+            </Container>
+            <Footer />
+        </Grid>
+    );
 };
 
 export default LoginPage;
--- packages/client/src/components/page/signupPage/index.jsx
+++ packages/client/src/components/page/signupPage/index.jsx
@@ -1,111 +1,171 @@
 import React, { useState } from "react";
 import {
-  Container,
-  Grid,
-  TextField,
-  Button,
-  Typography,
-  Card,
-  CardContent,
+    Container,
+    Grid,
+    TextField,
+    Button,
+    Typography,
+    Card,
+    CardContent,
 } from "@mui/material";
 import ResponsiveAppBar from "../../Nav-bar";
 import Footer from "../../footer";
 
 const SignUpPage = () => {
-  const [formData, setFormData] = useState({
-    username: "",
-    email: "",
-    password: "",
-    confirmPassword: "",
-  });
-
-  const handleInputChange = (e) => {
-    const { name, value } = e.target;
-    setFormData({
-      ...formData,
-      [name]: value,
+    const [formData, setFormData] = useState({
+        username: "",
+        fullname: "",
+        phone: "",
+        password: "",
+        confirmPassword: "",
     });
-  };
+    const [token, setToken] = useState(null);
+    const [errorMessage, setErrorMessage] = useState(null);
 
-  const handleSubmit = (e) => {
-    e.preventDefault();
-    // Perform sign-up logic or other necessary actions here
-    console.log("Form submitted with:", formData);
-    // Example: You might want to perform user registration here
-  };
+    const saveTokenToLocalStorage = (token) => {
+        localStorage.setItem("jwtToken", token);
+    };
+    const handleInputChange = (e) => {
+        const { name, value } = e.target;
+        setFormData({
+            ...formData,
+            [name]: value,
+        });
+    };
 
-  return (
-    <Grid>
-      <ResponsiveAppBar />
-      <Container maxWidth="xs">
-        <Card style={{ marginTop: "5rem", marginBottom: "5rem" }}>
-          <CardContent>
-            <Typography variant="h5" align="center" gutterBottom>
-              Sign Up
-            </Typography>
-            <form onSubmit={handleSubmit}>
-              <Grid container spacing={2}>
-                <Grid item xs={12}>
-                  <TextField
-                    fullWidth
-                    label="Username"
-                    name="username"
-                    variant="outlined"
-                    value={formData.username}
-                    onChange={handleInputChange}
-                  />
-                </Grid>
-                <Grid item xs={12}>
-                  <TextField
-                    fullWidth
-                    label="Email"
-                    name="email"
-                    type="email"
-                    variant="outlined"
-                    value={formData.email}
-                    onChange={handleInputChange}
-                  />
-                </Grid>
-                <Grid item xs={12}>
-                  <TextField
-                    fullWidth
-                    label="Password"
-                    name="password"
-                    type="password"
-                    variant="outlined"
-                    value={formData.password}
-                    onChange={handleInputChange}
-                  />
-                </Grid>
-                <Grid item xs={12}>
-                  <TextField
-                    fullWidth
-                    label="Confirm Password"
-                    name="confirmPassword"
-                    type="password"
-                    variant="outlined"
-                    value={formData.confirmPassword}
-                    onChange={handleInputChange}
-                  />
-                </Grid>
-                <Grid item xs={12}>
-                  <Button
-                    type="submit"
-                    fullWidth
-                    variant="contained"
-                    color="primary"
-                  >
-                    Sign Up
-                  </Button>
-                </Grid>
-              </Grid>
-            </form>
-          </CardContent>
-        </Card>
-      </Container>
-      <Footer />
-    </Grid>
-  );
+    const handleSubmit = async (e) => {
+        e.preventDefault();
+
+        try {
+            // Gọi API register từ server
+            const response = await fetch(
+                "http://localhost:8080/account/register",
+                {
+                    method: "POST",
+                    headers: {
+                        "Content-Type": "application/json",
+                    },
+                    body: JSON.stringify({
+                        username: formData.username,
+                        password: formData.password,
+                        name: formData.fullname,
+                        phone: formData.phone,
+                    }),
+                }
+            );
+
+            const data = await response.json();
+            if (!response.ok) {
+                if (response.status === 400) {
+                    // const dataerror = await response.json();
+                    //console.log(dataerror);
+                    setErrorMessage(data.message);
+                } else {
+                    throw new Error(`HTTP error! Status: ${response.status}`);
+                }
+            }
+
+            const mockToken = data.token;
+            setToken(mockToken);
+            saveTokenToLocalStorage(mockToken);
+            //chuyên trang
+        } catch (error) {
+            console.error("Error during registration:", error);
+        }
+    };
+
+    return (
+        <Grid>
+            <ResponsiveAppBar />
+            <Container maxWidth="xs">
+                <Card style={{ marginTop: "5rem", marginBottom: "5rem" }}>
+                    <CardContent>
+                        <Typography variant="h5" align="center" gutterBottom>
+                            Sign Up
+                        </Typography>
+                        <form onSubmit={handleSubmit}>
+                            <Grid container spacing={2}>
+                                <Grid item xs={12}>
+                                    <TextField
+                                        fullWidth
+                                        label="Username"
+                                        name="username"
+                                        variant="outlined"
+                                        value={formData.username}
+                                        onChange={handleInputChange}
+                                    />
+                                </Grid>
+
+                                <Grid item xs={12}>
+                                    <TextField
+                                        fullWidth
+                                        label="Fullname"
+                                        name="fullname"
+                                        variant="outlined"
+                                        value={formData.fullname}
+                                        onChange={handleInputChange}
+                                    />
+                                </Grid>
+
+                                <Grid item xs={12}>
+                                    <TextField
+                                        fullWidth
+                                        label="Phone"
+                                        name="phone"
+                                        type="phone"
+                                        variant="outlined"
+                                        value={formData.phone}
+                                        onChange={handleInputChange}
+                                    />
+                                </Grid>
+                                <Grid item xs={12}>
+                                    <TextField
+                                        fullWidth
+                                        label="Password"
+                                        name="password"
+                                        type="password"
+                                        variant="outlined"
+                                        value={formData.password}
+                                        onChange={handleInputChange}
+                                    />
+                                </Grid>
+                                <Grid item xs={12}>
+                                    <TextField
+                                        fullWidth
+                                        label="Confirm Password"
+                                        name="confirmPassword"
+                                        type="password"
+                                        variant="outlined"
+                                        value={formData.confirmPassword}
+                                        onChange={handleInputChange}
+                                    />
+                                </Grid>
+                                <Grid item xs={12}>
+                                    <Button
+                                        type="submit"
+                                        fullWidth
+                                        variant="contained"
+                                        color="primary"
+                                    >
+                                        Sign Up
+                                    </Button>
+                                    {errorMessage && (
+                                        <Typography
+                                            color="error"
+                                            variant="body2"
+                                        >
+                                            {errorMessage}
+                                        </Typography>
+                                    )}
+                                </Grid>
+                            </Grid>
+                        </form>
+                    </CardContent>
+                </Card>
+            </Container>
+            <Footer />
+        </Grid>
+    );
 };
 
 export default SignUpPage;
